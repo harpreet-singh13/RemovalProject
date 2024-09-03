abc894f7f2304371aa928c4ca99daf65782a12df
Singh <hss925957@tatatechnologies.com>
2024-09-03 10:17:49 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
+
+function App() {
+
+	return (
+		<>
+    <Navbar/>
+    <LandingPage/>
+
+    
+		</>
+	);
+}
+
+export default App;
